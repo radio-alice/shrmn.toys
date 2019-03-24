acfcdef800d28027591766c5ed042c0203798a5a
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-03-24 17:04:19 -0500

    fix collapse js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,16 @@
 var coll = document.getElementsByClassName("collapsible");
 
 window.onload = function() {
-  coll[0].addEventListener("click", function(event) {
-    this.classList.toggle("active");
-    console.log('clikc');
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+  for (let i=0; i < coll.length; i++){
+    coll[i].addEventListener("click", function(event) {
+      this.classList.toggle("active");
+      console.log('clikc');
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
 }
