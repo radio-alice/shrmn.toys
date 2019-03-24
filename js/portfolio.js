5c2f7abe731caa8b3d90bad2c0ee625cf97ecbbb
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-03-24 16:30:42 -0500

    onload strikes again

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,14 @@
 var coll = document.getElementsByClassName("collapsible");
-console.log(coll);
-coll[0].addEventListener("click", function(event) {
-  this.classList.toggle("active");
-  console.log('clikc');
-  var content = this.nextElementSibling;
-  if (content.style.display === "block") {
-    content.style.display = "none";
-  } else {
-    content.style.display = "block";
-  }
-});
+
+window.onload = function() {
+  coll[0].addEventListener("click", function(event) {
+    this.classList.toggle("active");
+    console.log('clikc');
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
