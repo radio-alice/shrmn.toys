bc897564071e84d24a96416eaed8d0c66ef3e6df
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-03-24 16:28:54 -0500

    hm

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,12 @@
 var coll = document.getElementsByClassName("collapsible");
 console.log(coll);
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function(event) {
-    this.classList.toggle("active");
-    console.log('clikc');
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+coll[0].addEventListener("click", function(event) {
+  this.classList.toggle("active");
+  console.log('clikc');
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+});
