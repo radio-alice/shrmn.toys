fa3a716cdf2223e01995593d5c3dc82641dc8111
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-03-24 21:57:06 -0500

    resign to not fixing videos

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,7 +4,6 @@ window.onload = function() {
   for (let i=0; i < coll.length; i++){
     coll[i].addEventListener("click", function(event) {
       this.classList.toggle("active");
-      console.log('clikc');
       var content = this.nextElementSibling;
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
