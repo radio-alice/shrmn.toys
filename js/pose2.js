45db71e6a99bb6035d5bccbb1e438916111731f6
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-04-03 10:49:19 -0500

    switch pose alg to single

--- js/pose2.js
+++ js/pose2.js
@@ -17,7 +17,7 @@
     video.size(width, height);
 
     // Create a new poseNet method with a single detection
-    poseNet = ml5.poseNet(video, 'multiple');
+    poseNet = ml5.poseNet(video);
     // This sets up an event that fills the global variable "poses"
     // with an array every time new poses are detected
     poseNet.on('pose', function(results) {
