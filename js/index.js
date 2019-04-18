7f9b926b07ce3e0cc2bd67eebb764ff142623003
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-04-17 20:22:13 -0500

    add fun URL effects

--- js/index.js
+++ js/index.js
@@ -1,29 +1,36 @@
-  window.onload = start;
-  var vid;
+window.onload = start;
+var vid;
 
-  function start () {
-    vid = document.getElementById('myVideo').style;
-  }
+function start () {
+  vid = document.getElementById('myVideo').style;
+}
 
-  function handler(e) {
-      e = e || window.event;
+function handler(e) {
+    e = e || window.event;
 
-      var pageX = e.pageX;
-      var pageY = e.pageY;
+    var pageX = e.pageX;
+    var pageY = e.pageY;
 
-      shift(pageX, pageY);
-  }
+    shift(pageX, pageY);
+}
 
-  // attach handler to the mouse event of the document
-  if (document.attachEvent) document.attachEvent('mousemove', handler);
-  else document.addEventListener('mousemove', handler);
+// attach handler to the mouse event of the document
+if (document.attachEvent) document.attachEvent('mousemove', handler);
+else document.addEventListener('mousemove', handler);
 
-  function shift(x, y){
-    var hue = (x / window.innerWidth) * 360;
-    var inv = ((y / window.innerHeight) * 20);
-      if (inv > 10){
-        inv += 90;
-      }
-    var blu = ((y / window.innerHeight) * 20) + 5;
-    vid.filter = "hue-rotate(" + hue + "deg) invert(" + inv + "%) blur(" + blu+ "px) saturate(500%)";
-  }
+function shift(x, y){
+  var hue = (x / window.innerWidth) * 360;
+  var inv = ((y / window.innerHeight) * 20);
+    if (inv > 10){
+      inv += 90;
+    }
+  var blu = ((y / window.innerHeight) * 20) + 5;
+  vid.filter = "hue-rotate(" + hue + "deg) invert(" + inv + "%) blur(" + blu+ "px) saturate(500%)";
+}
+
+var boys = ['🐵', '🙊', '🙉', '🙊', '🐒'];
+function urlGoof () {
+  location.hash = boys[Math.floor((Date.now()/100)%boys.length)];
+  setTimeout(urlGoof, 50);
+}
+urlGoof();
